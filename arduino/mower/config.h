45dbdfee5dfd39d60e55823521cb9c723d0a3af6
Kaptenfilling <62646887+Kaptenfilling@users.noreply.github.com>
2022-04-06 11:46:19 +0200

    Fine adjustments of declarations
    Co-Authored-By: Johnbombadill <62593587+Johnbombadill@users.noreply.github.com>
    Co-Authored-By: Sargis Tammo <90176669+sargistammo@users.noreply.github.com>

---
+++ arduino/mower/config.h
@@ -0,0 +1,7 @@
+//Calculated frequency in ms, deciding update frequency during serial communication.
+#define SERIAL_UPDATE_FREQUENCY_MS 1/20*1000
+
+//Sensor ports
+#define ULTRASONIC_SENSOR_PORT 10
+#define LINE_FOLLOWER_SENSOR_PORT 9
+#define 
--- arduino/mower/mower.ino
+++ arduino/mower/mower.ino
@@ -3,12 +3,21 @@
 #include <SoftwareSerial.h>
 #include <MeAuriga.h>
 #include <Math.h>
+#include "config.h"
 
-MeUltrasonicSensor ultrasonic_10(10);
+MeLineFollower linefollower_9(LINE_FOLLOWER_SENSOR_PORT);
+MeUltrasonicSensor ultrasonic_10(ULTRASONIC_SENSOR_PORT);
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
-MeLineFollower linefollower_9(9);
-MeLightSensor lightsensor_12(12);
+
+typedef enum {
+  FORWARD,
+  BACKWARD,
+  LEFT,
+  RIGHT } direction_t;
+
+float timeAtLastUpdate = 0;
+
 
 void isr_process_encoder1(void)
 {
@@ -26,20 +35,20 @@ void isr_process_encoder2(void)
     Encoder_2.pulsePosPlus();
   }
 }
-void move(int direction, int speed)
+void move(direction_t direction, int speed)
 {
   int leftSpeed = 0;
   int rightSpeed = 0;
-  if(direction == 1){
+  if(direction == FORWARD){
     leftSpeed = -speed;
     rightSpeed = speed;
-  }else if(direction == 2){
+  }else if(direction == BACKWARD){
     leftSpeed = speed;
     rightSpeed = -speed;
-  }else if(direction == 3){
+  }else if(direction == LEFT){
     leftSpeed = -speed;
     rightSpeed = -speed;
-  }else if(direction == 4){
+  }else if(direction == RIGHT){
     leftSpeed = speed;
     rightSpeed = speed;
   }
@@ -67,24 +76,43 @@ void setup() {
 
 }
 
+void getSerialData(){
+  if (millis() - timeAtLastUpdate > SERIAL_UPDATE_FREQUENCY_MS){
+    timeAtLastUpdate = millis();
+    Serial.println("update");
+  }
+}
+
+direction_t randomLeftOrRight(){
+  int leftOrRight = random(3, 5);
+
+  if (leftOrRight = 3){
+    return LEFT;
+  }else{
+    return RIGHT;
+  }
+}
+
 void autoMove(){
   int leftOrRight = random(3, 5);
   
   if(ultrasonic_10.distanceCm() < 10){
+      Serial.println("Ultrasonic sensor triggered");
       Encoder_1.setTarPWM(0);
       Encoder_2.setTarPWM(0);
       _delay(0.5);
 
-      move(2, 40 / 100.0 * 255);
+      move(BACKWARD, 40 / 100.0 * 255);
       _delay(1);
-      move(2, 0);
+      move(BACKWARD, 0);
 
-      move(leftOrRight, 30 / 100.0 * 255);
+      move(randomLeftOrRight(), 30 / 100.0 * 255);
       _delay(random(1, 3));
       move(leftOrRight, 0);
 
   }
   if(linefollower_9.readSensors() == 0.000000){
+      Serial.println("IR sensor triggered");
       Encoder_1.setTarPWM(0);
       Encoder_2.setTarPWM(0);
       _delay(0.5);
@@ -110,5 +138,6 @@ void _loop() {
 }
 
 void loop() {
+  getSerialData();
   autoMove();
 }
