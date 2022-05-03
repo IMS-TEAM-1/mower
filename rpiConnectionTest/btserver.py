4ba72dbf4bf708456e51ce255d6f4e7507f0a539
Kaptenfilling <62646887+Kaptenfilling@users.noreply.github.com>
2022-05-03 17:09:57 +0200

    Added bluetooth to main code

--- rpiConnectionTest/btserver.py
+++ rpiConnectionTest/btserver.py
@@ -1,42 +1,49 @@
+"""
+Bluetooth
+"""
+
 from bluetooth import*
-from arduino import Arduino
 
 import settings as s
 
-server_socket = BluetoothSocket(RFCOMM)
-server_socket.bind(("",PORT_ANY))
-server_socket.listen(1)
-
-ard     = Arduino(s.ARDUINO_SERIAL_DEV,
-                      s.ARDUINO_SERIAL_BAUD,
-                      s.ARDUINO_SERIAL_TIMEOUT)
-
-
-port = server_socket.getsockname()[1]
-
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-
-advertise_service( server_socket, "panzermower",
+class Btserver:
+    
+    def __init__(self, uuid):
+        self.server_socket = BluetoothSocket(RFCOMM)
+        self.server_socket.bind(("",PORT_ANY))
+        self.server_socket.listen(1)
+        self.port = self.server_socket.getsockname()[1]
+        
+        advertise_service( self.server_socket, "panzermower",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
                    profiles = [ SERIAL_PORT_PROFILE ], 
                     )
+        
+    def __del__(self):
+        pass
+    
+    def connect_to_app(self):
+        self.client_socket,self.client_info = self.server_socket.accept()
+        print("connected")
+        
+    def send_message(self, message):
+        #ard.send_message(message.decode('UTF-8'))
+        print(message)
+    
+    def recieve_message(self):
+        pass
+    
+    def recieve_message_from_app(self):
+        data = self.client_socket.recv(1024)
+        return data
+    
+    def close_connection(self):
+        client_socket.close()
+        print("disconnected")
+        server_socket.close()
 
-client_socket,client_info =server_socket.accept()
-print("connected")
-try:
-    while True:
-        data = client_socket.recv(1024)
-        if len(data) == 0: break
-        else:
-            ard.send_message(data.decode('UTF-8'))
-            #send_message(data)
 
 
-except IOError:
-    pass
 
 
-client_socket.close()
-print("disconnected")
-server_socket.close()
--- rpiConnectionTest/main.py
+++ rpiConnectionTest/main.py
@@ -4,6 +4,7 @@ from time import sleep
 from arduino import Arduino
 from camera  import Camera
 from backend import Backend
+from btserver import Btserver
 
 import settings as s
 
@@ -19,11 +20,13 @@ if __name__ == '__main__':
                       s.ARDUINO_SERIAL_BAUD,
                       s.ARDUINO_SERIAL_TIMEOUT)
 
-    #cam     = Camera(s.CAMERA_SCREENSHOT_RESOLUTION,
-    #                 s.CAMERA_DIRECTORY)
+#    cam     = Camera(s.CAMERA_SCREENSHOT_RESOLUTION,
+#                     s.CAMERA_DIRECTORY)
 
     backend = Backend(s.BACKEND_URI,
                       s.BACKEND_PORT)
+    
+    btserver = Btserver(s.BLUETOOTH_UUID)
 
     # state = MANUAL
 
@@ -40,16 +43,26 @@ if __name__ == '__main__':
 
     # Start communicating                   
     # ard.hello()
+    
+    btserver.connect_to_app()
+    bluetooth_active = True
 
     while True:
+        while(bluetooth_active): #ersätt med status från backend
+            data = btserver.recieve_message_from_app()
+            if (len(data) != 0):
+                #btserver.send_message(data)
+                ard.send_message(data.decode('UTF-8'))
+        
+        
         #print(ard.receive_message())
-        if ard.receive_message() == "CAPTURE":
-            Camera.open_sleep_capture_close(s.CAMERA_SCREENSHOT_RESOLUTION, s.CAMERA_DIRECTORY)
-            #cam.capturePicture()
-            ard.send_message("CAPTURE:ack")
-            print("CAPTURE:ack sent")
-            #print(ard.receive_message())
-        sleep(1)
+#        if ard.receive_message() == "CAPTURE":
+#            Camera.open_sleep_capture_close(s.CAMERA_SCREENSHOT_RESOLUTION, s.CAMERA_DIRECTORY)
+#            #cam.capturePicture()
+#            ard.send_message("CAPTURE:ack")
+#            print("CAPTURE:ack sent")
+#            #print(ard.receive_message())
+#        sleep(1)
 
     #     ard.post()
     #     sleep(1)
